0bf0b5ed1b6bafa875aa104acb4a1557e43e5380
Krishna Murthy <krrish94@gmail.com>
2018-07-30 16:24:32 -0400

    Model and DataLoaders refactored

--- lieFunctions.py
+++ lieFunctions.py
@@ -12,6 +12,37 @@ import numpy as np
 # def SO3_log(R):
 
 
+# Convert a rotation matrix to an axis-angle vector. Does vee(SO3_log(rot))
+def rotMat_to_axisAngle(rot):
+
+	trace = rot[0,0] + rot[1,1] + rot[2,2]
+	trace = np.clip(trace, 0.0, 2.99999)
+	theta = np.arccos((trace - 1.0)/2.0)
+	omega_cross = (theta/(2*np.sin(theta)))*(rot - np.transpose(rot))
+
+	return [omega_cross[2,1], omega_cross[0,2], omega_cross[1,0]]
+
+
+# Map an axis-angle vector to a rotation matrix. Does SO3_exp(hat(omega))
+def axisAngle_to_rotMat(omega):
+
+	theta = np.sqrt(omega[0]*omega[0] + omega[1]*omega[1] + omega[2]*omega[2])
+
+	if theta < 1e-8:
+		return np.eye(3,3)
+
+	omega_cross = np.stack([0.0, -omega[2], omega[1], omega[2], 0.0, -omega[0], -omega[1], omega[0], 0.0])
+	omega_cross = np.reshape(omega_cross, [3,3])
+
+	A = np.sin(theta)/theta
+	B = (1.0 - np.cos(theta))/(theta**2)
+	C = (1.0 - A)/(theta**2)
+
+	omega_cross_square = np.matmul(omega_cross, omega_cross)
+	R = np.eye(3,3) + A * omega_cross + B * omega_cross_square
+	return R
+
+
 # Convert a 3 x 3 rotation matrix to a quaternion
 # Abridged from the elegant pyquaternion library 
 # https://github.com/KieranWynn/pyquaternion/
